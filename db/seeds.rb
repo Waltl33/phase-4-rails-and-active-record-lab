70c67b93dc2b3cd8436417d3433086addc420260
Walter <walter.clark24@yahoo.com>
2023-02-18 13:15:28 -0500

    commit tree 00afbed4054c12afb8ef9dbfe72eeb7a5c3f7585

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "John", last_name: "Judy", grade:5)
+Student.create(first_name: "Jim", last_name: "Clark", grade:5)
+Student.create(first_name: "Vill", last_name: "man", grade:5)
+Student.create(first_name: "Fred", last_name: "Judy", grade:5)
